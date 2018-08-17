e8cfc1415476d5c322564b83f3fe4669e95375c0
ELIZABETH ALVARADO <lily21418@yahoo.com>
2018-08-17 05:35:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
     console.log("I can't hear you!") 
     
   }
-  else if (string === "HELLO") { 
+  else if (string === string.ro) { 
     console.log("YES INDEED!") 
     
   } 
